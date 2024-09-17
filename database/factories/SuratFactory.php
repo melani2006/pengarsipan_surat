ebf83616bec43a5f48dde013c40e4403203825ab
melani2006 <melaniherlitaputri97@gmail.com>
2024-09-17 08:42:26 +0700

    bebas

--- database/factories/SuratFactory.php
+++ database/factories/SuratFactory.php
@@ -2,39 +2,34 @@
 
 namespace Database\Factories;
 
-use App\Models\Surat;
+use App\Enums\LetterType;
+use App\Models\Surat; 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
-use Carbon\Carbon;
 
+/**
+ * @extends Factory<Surat>
+ */
 class SuratFactory extends Factory
 {
     /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
-    protected $model = Surat::class;
-
-    /**
      * Define the model's default state.
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public function definition(): array
     {
         return [
-            'reference_number' => $this->faker->unique()->numerify('REF-#####'),
-            'agenda_number' => $this->faker->unique()->numerify('AGENDA-#####'),
-            'from' => $this->faker->company(),
-            'penerima' => $this->faker->company(),
-            'Tanggal_Surat' => $this->faker->date(),
-            'Tanggal_Diterima' => $this->faker->date(),
-            'deskripsi' => $this->faker->sentence(),
-            'Catatan' => $this->faker->optional()->sentence(),
-            'type' => $this->faker->randomElement(['INCOMING', 'OUTGOING']),
-            'kategoris_code' => $this->faker->randomElement(['CODE1', 'CODE2', 'CODE3']),
-            'user_id' => \App\Models\User::factory(), // Asumsi bahwa user factory sudah ada
+            'nomor_surat' => $this->faker->ean13(), 
+            'nomor_agenda' => $this->faker->randomNumber(5), 
+            'pengirim' => $this->faker->name('male'), 
+            'penerima' => $this->faker->name('female'), 
+            'Tanggal_Surat' => $this->faker->date(), 
+            'Tanggal_Diterima' => $this->faker->date(), 
+            'deskripsi' => $this->faker->sentence(7), 
+            'catatan' => $this->faker->sentence(3),
+            'type' => $this->faker->randomElement([LetterType::INCOMING->type(), LetterType::OUTGOING->type()]),
+            'kategori_code' => 'ADM', 
+            'user_id' => 1, 
         ];
     }
 }
